d956ee029ef92d08de0f946062a73bc23fde2c35
Abdullah Amin <bido6573@gmail.com>
2023-12-06 00:37:54 +0200

    finish edits on condition activity

--- app/src/main/java/com/example/hope_uj_clinic/my_flow/ConditionClassifierActivity.java
+++ app/src/main/java/com/example/hope_uj_clinic/my_flow/ConditionClassifierActivity.java
@@ -4,19 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 
 import com.example.hope_uj_clinic.R;
+import com.example.hope_uj_clinic.databinding.ActivityConditionClassifierBinding;
 
 public class ConditionClassifierActivity extends AppCompatActivity {
+
+    private ActivityConditionClassifierBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_condition_classifier);
+        binding = ActivityConditionClassifierBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     public void classify(View view) {
+
+        if ((binding.HREt.getText().toString().isEmpty() || !isNumeric(binding.HREt.getText().toString()))) {
+            binding.HREt.setError("Please enter a valid number");
+            return;
+        }
+        if ((binding.oxygenEt.getText().toString().isEmpty() || !isNumeric(binding.oxygenEt.getText().toString()))) {
+            binding.oxygenEt.setError("Please enter a valid number");
+            return;
+        }
+        if ((binding.respEt.getText().toString().isEmpty() || !isNumeric(binding.respEt.getText().toString()))) {
+            binding.respEt.setError("Please enter a valid number");
+            return;
+        }
+        if ((binding.tempEt.getText().toString().isEmpty() || !isNumeric(binding.tempEt.getText().toString()))) {
+            binding.tempEt.setError("Please enter a valid number");
+            return;
+        }
+
         startActivity(new Intent(ConditionClassifierActivity.this, ConditionActivity.class));
+    }
+
+    private boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
     }
 }
